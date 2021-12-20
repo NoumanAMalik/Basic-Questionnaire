2b788e4efc54fe18420c04ea33708d5d6ab5df81
Nouman <noumancodes@gmail.com>
2021-12-19 21:01:37 -0500

    Add: Added questions

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How long is the Great Wall of China?',
+        a: '12345.67 miles',
+        b: '13170.58 miles',
+        c: '26384.75 miles',
+        d: '11542.21 miles',
+        correct: 'b'
+    }, {
+        question: 'How much of the world did Great Britian own at its peak?',
+        a: '15%',
+        b: '20%',
+        c: '25%',
+        d: '30%',
+        correct: 'c'
+    }, {
+        question: 'Who painted the Mona Lisa?',
+        a: 'Leonardo da Vinci',
+        b: 'Leonardo DiCaprio',
+        c: 'Michelangelo',
+        d: 'Vincent van Gogh',
+        correct: 'a'
+    }, {
+        question: 'What is the biggest Box Office Movie?',
+        a: 'Avatar',
+        b: 'Avengers: Endgame',
+        c: 'Titanic',
+        d: 'Star Wars: Episode VII - The Force Awakens',
+        correct: 'a'
+    }, {
+        question: 'How tall is Shaquille O\'Neal',
+        a: '6\'10',
+        b: '6\'11',
+        c: '7',
+        d: '7\'1',
+        correct: 'c'
+    }
+]
